3418252250f86152cf48543ff60c322be061c726
Adarsh Chandran <adarsh.chndrn@gmail.com>
2016-02-25 11:36:25 +0530

    corrected intendation
    Changed tab spaces to 4 space intendation

--- Array-to-BST.rb
+++ Array-to-BST.rb
@@ -1,27 +1,27 @@
 def create_node input
-	return {:left => nil, :value => input, :right => nil}
+    return {:left => nil, :value => input, :right => nil}
 end
 
 def add_node current_node, new_node
-	if new_node[:value] < current_node[:value]
-		if current_node[:left] == nil
-			current_node[:left] = new_node
-		else
-			add_node current_node[:left], new_node
-		end
-	elsif new_node[:value] > current_node[:value]
-		if current_node[:right] == nil
-			current_node[:right] = new_node
-		else
-			add_node current_node[:right], new_node
-		end
-	end
+    if new_node[:value] < current_node[:value]
+        if current_node[:left] == nil
+            current_node[:left] = new_node
+        else
+            add_node current_node[:left], new_node
+        end
+    elsif new_node[:value] > current_node[:value]
+        if current_node[:right] == nil
+            current_node[:right] = new_node
+        else
+            add_node current_node[:right], new_node
+        end
+    end
 end
 
 def array_to_binary array
-	array.map!{|node| create_node node}
-	root = array[0]
-	array.each{|node| add_node root, node}
+    array.map!{|node| create_node node}
+    root = array[0]
+    array.each{|node| add_node root, node}
 end
 
 array = [8,2,9,11,5]
